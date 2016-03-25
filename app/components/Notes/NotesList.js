75aa6f44f99347266781836448b339567a75d35d
injune1123 <injune1123@Juns-MacBook-Air.local>
2016-03-25 13:57:29 -0400

    refactored the code of notes components

--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -1,17 +1,18 @@
-var React = require('react');
+import React from 'react';
 
-var NoteList = React.createClass({
-	render: function(){
-		var notes = this.props.notes.map(function(note,index){
-			return <li className="list-group-item" key={index}>{note[".value"]}</li>
-		})
+class NotesList extends React.Component{
+	render(){
+		const { notes } = this.props;
 		return (
-		<ul className="list-group">
-			{notes}
-		</ul>	
+			<ul className="list-group">
+				{notes.map((note,index) => (
+					<li className="list-group-item" key={index}>{note[".value"]}</li>
+				))}
+			</ul>	
 		)
+
 	}
+};
 
-});
 
-module.exports = NoteList;
+export default NotesList;
